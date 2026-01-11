38246ae694a3f273d5555b3295334d7883d0a252
Bibek-Poudel8 <bibekeight8@gmail.com>
2026-01-11 21:05:25 +0545

    updates to note app

--- app/api/notes/[id]/route.jsx
+++ app/api/notes/[id]/route.jsx
@@ -3,6 +3,27 @@ import Note from "@/models/Note";
 import { NextResponse } from "next/server";
 
 
+export async function PUT(request, { params }) {
+try {
+    
+    const { id } = await params;
+    await dbConnect();
+    const body = await request.json();
+    const note = await Note.findByIdAndUpdate(id,
+         {...body, updatedAt: Date.now() },
+            { new: true , runValidators: true }
+    );
+    if (!note) {
+        return NextResponse.json({ success:false, error: "Note not found" }, { status: 404 });
+    }
+    return NextResponse.json({ success:true, message: "Note updated successfully", data: note }, { status: 200 });
+} catch (error) {
+    return NextResponse.json({ success:false, message: "Failed to update note", error: error.message }, { status: 500 });
+}
+
+}
+
+
 export async function DELETE(request, { params }) {
     try {
         const { id } = await params;
